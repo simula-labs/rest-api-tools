dc8ffbded62c5e1a8869f1fe53f66f8948041228
mackeee-orange <makino.aaa@gmail.com>
2022-05-19 16:43:15 +0900

    feat: BaseRequestからreact-queryに渡せる用の関数を生成できるように

--- lib/error.ts
+++ lib/error.ts
@@ -2,10 +2,12 @@ export class ClientError extends Error {}
 
 export class NetworkError extends Error {
   status: number;
+  customErrorCode?: string | number;
 
-  constructor(status: number, message?: string) {
+  constructor(status: number, message?: string, errorCode?: string | number) {
     super(message);
     this.name = new.target.name;
     this.status = status;
+    this.customErrorCode = errorCode;
   }
 }
